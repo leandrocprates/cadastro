c9ca3d9e2ae41515986a7660ea18c9928a07f91d
leandro prates <leandro.prates@gmail.com>
2020-05-17 15:06:01 -0300

    projeto de cadastro - colocado a biblioteca do swagger para documentacao

--- src/main/java/com/example/cadastro/config/SwaggerConfig.java
+++ src/main/java/com/example/cadastro/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.example.cadastro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +25,30 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+                //.useDefaultResponseMessages(false)
+                //.globalResponseMessage(RequestMethod.GET, responseMessageForGET()) ;
     }
+
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("Internal Error ")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(404)
+                    .message("Not Found!")
+                    .build());
+        }};
+    }
+
+
 
 }
--- src/main/java/com/example/cadastro/controller/UsuarioController.java
+++ src/main/java/com/example/cadastro/controller/UsuarioController.java
@@ -3,10 +3,14 @@ package com.example.cadastro.controller;
 import com.example.cadastro.dto.UsuarioDTO;
 import com.example.cadastro.model.Usuario;
 import com.example.cadastro.service.UsuarioService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +25,25 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService ;
 
-    @RequestMapping(value="", method = RequestMethod.GET)
+    @ApiOperation(value = "Retorna a lista de Usuarios." )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna a lista de Usuarios"),
+            @ApiResponse(code = 404, message = "Nenhum Usuario Encontrado"),
+            @ApiResponse(code = 500, message = "Erro Interno no Servidor")
+    })
+    @RequestMapping(value="", method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Usuario>> listarTodos(){
         List<Usuario> listUsuarios = usuarioService.listarTodos();
         return new ResponseEntity<>(listUsuarios , HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{id}" , method = RequestMethod.GET)
+    @ApiOperation(value = "Retorno o usuario pelo id informado.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna o usuario pelo Id informado"),
+            @ApiResponse(code = 404, message = "Usuario nao Encontrado"),
+            @ApiResponse(code = 500, message = "Erro Interno no Servidor")
+    })
+    @RequestMapping(value = "/{id}" , method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UsuarioDTO> getUsuarioById(@PathVariable Long id){
         UsuarioDTO usuarioDTO =  usuarioService.getUsuarioById(id);
         return new ResponseEntity<UsuarioDTO>( usuarioDTO, HttpStatus.OK ) ;
@@ -48,12 +64,23 @@ public class UsuarioController {
      * }
      *
     */
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ApiOperation(value="Salva um usuario . Retornando o usuario com o Id gerando no banco.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200 , message = "Salva usuario no Banco de Dados "),
+            @ApiResponse(code = 500, message = "Erro Interno no Servidor")
+    })
+    @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDTO){
         UsuarioDTO usuarioDTOCreated =  usuarioService.salvar(usuarioDTO);
         return new ResponseEntity<UsuarioDTO>(usuarioDTOCreated, HttpStatus.CREATED );
     }
 
+    @ApiOperation(value = "Deleta usuario pelo Id informado.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Excluir usuario pelo ID informado"),
+            @ApiResponse(code = 404, message = "Usuario nao Encontrado"),
+            @ApiResponse(code = 500, message = "Erro Interno no Servidor")
+    })
     @RequestMapping(value="/{id}",method=RequestMethod.DELETE)
     public ResponseEntity<Object> deleleById(@PathVariable  Long id){
         usuarioService.deleteById(id);
