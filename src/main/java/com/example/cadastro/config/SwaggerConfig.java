0017c65c4dd2b7d7f70031c75aa0dd338fa049cd
leandro prates <leandro.prates@gmail.com>
2020-05-17 17:08:27 -0300

    projeto de cadastro - colocado a biblioteca do swagger para documentacao

--- src/main/java/com/example/cadastro/config/SwaggerConfig.java
+++ src/main/java/com/example/cadastro/config/SwaggerConfig.java
@@ -3,10 +3,13 @@ package com.example.cadastro.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -22,9 +25,10 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.cadastro.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
                 //.useDefaultResponseMessages(false)
                 //.globalResponseMessage(RequestMethod.GET, responseMessageForGET()) ;
     }
@@ -49,6 +53,17 @@ public class SwaggerConfig {
         }};
     }
 
+    private ApiInfo apiInfo() {
 
+        return new ApiInfoBuilder()
+                .title("Cadastro API")
+                .description("Projeto de Cadastro API")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Leandro Cesar Prates", "https://github.com/leandrocprates/", "leandro.prates@gmail.com"))
+                .build();
+
+    }
 
 }
