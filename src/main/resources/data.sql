dba23a4dd4ae6fcd7e7402d903aacf2d8d0b3241
leandro prates <leandro.prates@gmail.com>
2021-01-30 17:35:56 -0300

    sql data insert tabela usuario

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+--USE cadastro;
+
+INSERT INTO usuario (id, nome, ativo, data) VALUES (1,'Leandro',1,'2020-04-19 12:35:26');
+INSERT INTO usuario (id, nome, ativo, data) VALUES (2,'Ariane',0,'2020-04-19 12:35:26');
+INSERT INTO endereco (id, cep, endereco, bairro, usuario_id) VALUES (30,'01040-060','Avenida Paulista','Bela Vista',1);
+
+
